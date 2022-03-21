0a354aa467c4f73d4c8fc42af4589e0153e1b8e5
SEDO11 <do11anm@naver.com>
2022-03-21 10:00:39 +0900

    220321

---
+++ baekjoon/Exam2525/Exam2525/Program.cs
@@ -0,0 +1,33 @@
+﻿//ai 오븐
+using System;
+
+namespace Exam2525
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String timeStr = Console.ReadLine(); // 한줄에 공백으로 구분해서 시간과 분을 입력 받음
+            String[] time = timeStr.Split(' '); // 입력 받은 시간을 공백을 기준으로 시간과 분으로 나눔
+            int setTime = Convert.ToInt32(Console.ReadLine()); // 몇분 돌릴껀지 설정
+            int hour = Convert.ToInt32(time[0]); // 시간
+            int minute = Convert.ToInt32(time[1]); // 분
+            int setMin = 60;
+            int setHour = 24;
+
+            minute += setTime; // 분끼리 계산
+            //setTime /= 60;
+            if (minute >= setMin) // 분이 60분 이상일 경우
+            {
+                hour += minute / setMin; // 분의 몫을 시간에 추가 해줌
+                minute %= setMin; // 나머지를 분에 대입해줌
+                if(hour >= setHour)
+                {
+                    hour %= setHour;
+                }
+            }
+
+            Console.WriteLine($"{hour} {minute}");
+        }
+    }
+}
---
+++ by_star/by_star/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace by_star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("0부터 몇까지 숫자를 출력 하시겠습니까?> ");
+            int input = Convert.ToInt32(Console.ReadLine()); //문자열로 입력받아 정수형으로 변환 java의 Integer.parseInt와 같음
+            String result = null; // string 값 초기화
+            for(int i=1; i<=input; i++)
+            {
+                if(i%3==0)
+                {
+                    result += "*";
+                }
+                else
+                {
+                    result += i;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
