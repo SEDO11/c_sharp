03a966471db82c95d72f82a0dee009a47859381c
SEDO11 <do11anm@naver.com>
2022-03-30 16:55:38 +0900

    220330

---
+++ list_/list_/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> items = new List<string>();
+            items.Add("홍길동");
+            items.Add("일지매");
+            items.Add("강동원");
+            
+            for(int i=0; i<items.Count; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        
+            
+        }
+
+    }
+}
