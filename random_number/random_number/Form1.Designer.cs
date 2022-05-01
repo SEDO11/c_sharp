4d80880b1dc8067cba43b78859bf182df3c5054a
SEDO11 <do11anm@naver.com>
2022-05-01 21:35:54 +0900

    220501

---
+++ random_number/random_number/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace random_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.printLabel = new System.Windows.Forms.Label();
+            this.remainNumber = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_len = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.canvasPictureBox = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn.AutoSize = true;
+            this.btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn.Location = new System.Drawing.Point(250, 390);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(169, 85);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "추첨";
+            this.btn.UseMnemonic = false;
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(210, 524);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 100);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // printLabel
+            // 
+            this.printLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.printLabel.AutoSize = true;
+            this.printLabel.BackColor = System.Drawing.Color.GreenYellow;
+            this.printLabel.Font = new System.Drawing.Font("굴림", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.printLabel.Location = new System.Drawing.Point(110, 61);
+            this.printLabel.Name = "printLabel";
+            this.printLabel.Size = new System.Drawing.Size(456, 96);
+            this.printLabel.TabIndex = 3;
+            this.printLabel.Text = "추첨 번호";
+            this.printLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.printLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // remainNumber
+            // 
+            this.remainNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.remainNumber.AutoSize = true;
+            this.remainNumber.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.remainNumber.Location = new System.Drawing.Point(514, 601);
+            this.remainNumber.Name = "remainNumber";
+            this.remainNumber.Size = new System.Drawing.Size(65, 12);
+            this.remainNumber.TabIndex = 5;
+            this.remainNumber.Text = "남은 개수: ";
+            this.remainNumber.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(587, 524);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(70, 21);
+            this.textInput.TabIndex = 6;
+            this.textInput.TextChanged += new System.EventHandler(this.textInput_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(496, 533);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "번호 개수 입력";
+            // 
+            // btn_len
+            // 
+            this.btn_len.BackColor = System.Drawing.Color.Transparent;
+            this.btn_len.Location = new System.Drawing.Point(587, 566);
+            this.btn_len.Name = "btn_len";
+            this.btn_len.Size = new System.Drawing.Size(75, 23);
+            this.btn_len.TabIndex = 8;
+            this.btn_len.Text = "입력";
+            this.btn_len.UseVisualStyleBackColor = false;
+            this.btn_len.Click += new System.EventHandler(this.btn_len_Click);
+            // 
+            // canvasPictureBox
+            // 
+            this.canvasPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvasPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvasPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.canvasPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.canvasPictureBox.Name = "canvasPictureBox";
+            this.canvasPictureBox.Size = new System.Drawing.Size(684, 661);
+            this.canvasPictureBox.TabIndex = 9;
+            this.canvasPictureBox.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Green;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.btn_len);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.remainNumber);
+            this.Controls.Add(this.printLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.canvasPictureBox);
+            this.Name = "Form1";
+            this.Text = "숫자 랜덤 뽑기 프로그램";
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label printLabel;
+        private System.Windows.Forms.Label remainNumber;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_len;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox canvasPictureBox;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ random_number/random_number/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace random_number
+{
+    public partial class Form1 : Form
+    {
+        List<int> list = new List<int>();
+        int n = 0;
+
+        private float gradientStart = 0;
+
+        private float delta = 5f;
+        public Form1()
+        {
+            InitializeComponent();
+            list.Add(1);
+            label1.Text = "1~n까지 랜덤 출력\n실행 방법\n스페이스바 or 버튼 클릭";
+
+            printLabel.BackColor = Color.Transparent;
+            printLabel.Parent = canvasPictureBox;
+            label1.BackColor = Color.Transparent;
+            label1.Parent = canvasPictureBox;
+            label2.BackColor = Color.Transparent;
+            label2.Parent = canvasPictureBox;
+            remainNumber.BackColor = Color.Transparent;
+            remainNumber.Parent = canvasPictureBox;
+
+            #region 무빙 이벤트를 설정한다.
+            this.canvasPictureBox.Paint += canvasPictureBox_Paint; 
+            this.timer.Tick += timer_Tick;
+            #endregion
+
+        }
+
+        public void CreateList(int l) // 지연
+        {
+            for (int i = 2; i <= l; i++) // 리스트 값 생성
+            {
+                list.Add(i);
+            }
+            Shuffle(list);
+        }
+
+        public static List<T> Shuffle<T>(List<T> list) // 리스트 값 셔플
+        {
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                Random random = new Random(Guid.NewGuid().GetHashCode());
+                int rnd = random.Next(0, i);
+                T temp = list[i];
+                list[i] = list[rnd];
+                list[rnd] = temp;
+            }
+            return list;
+        }
+
+        public void Delay(int ms) // 지연
+        {
+            DateTime dateTimeNow = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, ms);
+            DateTime dateTimeAdd = dateTimeNow.Add(duration);
+            while (dateTimeAdd >= dateTimeNow)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                dateTimeNow = DateTime.Now;
+            }
+            return;
+        }
+
+        private void btn_Click(object sender, EventArgs e) // 버튼 클릭
+        {
+            playSimpleSound();
+            if (n < list.Count)
+            {
+                printLabel.Text = "추첨 번호\n" + list[0];
+                remainNumber.Text = "남은 개수: " + (list.Count - 1);
+                list.RemoveAt(0);
+            }
+            else
+            {
+                printLabel.Text = "종료";
+                Delay(2000);
+                Close();
+            }
+        }
+
+        private void playSimpleSound() // 소리 재생
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+            simpleSound.Play();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_len_Click(object sender, EventArgs e) // 값 입력
+        {
+            CreateList(Int32.Parse(textInput.Text));
+            textInput.Text = "";
+        }
+
+        private void textInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // 배경 움직임
+        private void canvasPictureBox_Paint(object sender, PaintEventArgs e) 
+        { 
+            int width = this.canvasPictureBox.ClientSize.Width; 
+            SetBackgroundImage(e.Graphics, this.gradientStart, this.gradientStart + width); 
+            this.gradientStart += this.delta; 
+            if (this.gradientStart >= width) 
+            { 
+                this.gradientStart = 0; 
+            } 
+            
+            using (Font font = new Font("Times New Roman", 32, FontStyle.Bold)) 
+            {
+                using (StringFormat stringFormat = new StringFormat()) 
+                {
+                    stringFormat.Alignment = StringAlignment.Center; 
+                    stringFormat.LineAlignment = StringAlignment.Center; 
+                    e.Graphics.DrawString(
+                        "",
+                        font, 
+                        Brushes.Black, 
+                        this.canvasPictureBox.ClientSize.Width / 2, 
+                        this.canvasPictureBox.ClientSize.Height / 2, 
+                        stringFormat); 
+                } 
+            } 
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.canvasPictureBox.Refresh();
+        }
+
+        private void SetBackgroundImage(Graphics graphics, float minimumX, float maximumX) 
+        { 
+            using (LinearGradientBrush brush = new LinearGradientBrush(new PointF(minimumX, 0), 
+                new PointF(maximumX, 0), Color.Green, Color.Green)) 
+            { 
+                brush.WrapMode = WrapMode.Tile; 
+                ColorBlend colorBlend = new ColorBlend(); 
+                colorBlend.Colors = new Color[] 
+                { Color.Green, Color.DarkGray, Color.Green }; 
+                colorBlend.Positions = new float[] { 0, 0.8f, 1}; 
+                brush.InterpolationColors = colorBlend; 
+                graphics.FillRectangle(brush, this.canvasPictureBox.ClientRectangle); 
+            } 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        }
+    }
+}
